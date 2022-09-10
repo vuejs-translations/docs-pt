a7aef0977de1dac0bc74dc0ae2e62acc3cd0e9b8
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-09-10 17:45:01 +0100

    chore: add `src/tutorial/` folder

---
+++ src/tutorial/src/step-2/App/composition.js
@@ -0,0 +1,12 @@
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // component logic
+    // declare some reactive state here.
+
+    return {
+      // exposed to template
+    }
+  }
+}
---
+++ src/tutorial/src/step-2/App/options.js
@@ -0,0 +1,4 @@
+export default {
+  // component options
+  // declare some reactive state here.
+}
