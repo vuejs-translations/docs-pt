0acc609e0054ec1873a2c1682d78242d76cd4fb2
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-13 09:39:10 +0100

    docs(pt): translate `examples/svg`

--- src/examples/src/svg/PolyGraph/options.js
+++ src/examples/src/svg/PolyGraph/options.js
@@ -9,7 +9,7 @@ export default {
     stats: Array
   },
   computed: {
-    // a computed property for the polygon's points
+    // uma propriedade computada para os pontos do polígono
     points() {
       const total = this.stats.length
       return this.stats
