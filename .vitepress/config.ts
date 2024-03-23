815334ee115201f9fe5309f588497bf17291b4f9
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-03-23 20:18:05 +0100

    docs: add translation link to `https://fa.vuejs.org`

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -666,6 +666,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-it'
       },
       {
+        link: 'https://fa.vuejs.org',
+        text: 'فارسی',
+        repo: 'https://github.com/vuejs-translations/docs-fa'
+      },
+      {
         link: '/translations/',
         text: 'Ajude-Nos a Traduzir!',
         isTranslationsDesc: true
